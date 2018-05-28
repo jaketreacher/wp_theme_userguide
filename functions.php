21e8c0709981d22794c6456d715239c99bc307a8
Jake Treacher <git@jaketreacher.com>
2018-05-28 21:15:15 +0800

    Add CSS, JS and Menus

--- functions.php
+++ functions.php
@@ -1,5 +1,40 @@
 <?php
 
+add_theme_support( 'html5' );
+
+/**
+ * Add CSS
+ */
+function theme_styles() {
+    wp_enqueue_style( 'bootstrap_css', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css' );
+    wp_enqueue_style( 'main_css', get_template_directory_uri() . '/style.css' );
+}
+add_action( 'wp_enqueue_scripts', 'theme_styles' );
+
+/**
+ * Add JS
+ */
+function theme_js() {
+    global $wp_scripts;
+
+    wp_enqueue_script( 'jquery', 'https://code.jquery.com/jquery-3.3.1.slim.min.js' );
+    wp_enqueue_script( 'popper', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js' );
+    wp_enqueue_script( 'bootstrap_js', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js' );
+    // wp_enqueue_script( 'main_js', get_template_directory_uri() . '/script.js');
+}
+add_action( 'wp_enqueue_scripts', 'theme_js' );
+
+/**
+ * Register Menu Locations
+ */
+register_nav_menus([
+    'main-menu' => esc_html__( 'Main Menu – Displayed at the top of the Pgae', 'userguide' ),
+    'guide-menu' => esc_html__( 'Guide Menu – Displayed to the left of the User Guide page', 'userguide' )
+]);
+
+/**
+ * Register Guide Custom Post Type
+ */
 function create_guide() {
     $labels = array(
         'name' => __( 'Guides' ),
@@ -17,7 +52,6 @@ function create_guide() {
         'public' => true,
         'menu_icon' => 'dashicons-book',
     );
-    register_post_type( 'guides', $args );
+    register_post_type( 'guide', $args );
 }
-
 add_action( 'init', 'create_guide' );
