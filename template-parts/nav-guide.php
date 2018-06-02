704295abb119e91c6a31ca56f72bbb62b9bb7c6a
Jake Treacher <git@jaketreacher.com>
2018-06-02 19:41:46 +0800

    Add basic style and nonfunctioning toggle button

--- template-parts/nav-guide.php
+++ template-parts/nav-guide.php
@@ -1,7 +1,11 @@
-<?php
-    wp_nav_menu([
-        'theme_location'    => 'guide-menu',
-        'menu_class'        => 'nav nav-pills flex-column',
-        'walker'            => new UserGuide_Navwalker(),
-    ]);
-?>
+<div class="guide-menu-container">
+    <span class="toggle">X</span>
+    <?php
+        wp_nav_menu([
+            'theme_location'    => 'guide-menu',
+            'container'         => null,
+            'menu_class'        => 'nav nav-pills flex-column',
+            'walker'            => new UserGuide_Navwalker(),
+        ]);
+    ?>
+</div>
